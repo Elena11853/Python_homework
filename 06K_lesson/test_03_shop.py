5f52b36c8974effa371adbcd93d8dd857fecd5ea
Elena <beklin24@gmail.com>
2025-03-27 00:11:27 +0300

    Покупка

--- 06K_lesson/test_03_shop.py
+++ 06K_lesson/test_03_shop.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -13,17 +12,45 @@ wait = WebDriverWait(driver, 10)
 #Вход на сайт
 driver.get("https://www.saucedemo.com/")
 
-# Вводим задержку
-Username = driver.find_element(By.ID, '#user-name').send_keys('standard_user')
-Password = driver.find_element(By.ID, '#password').send_keys('secret_sauce')
-login_button = driver.find_element(By.ID, '#login-button')
+# Авторизация
+Username = driver.find_element(By.ID, 'user-name').send_keys('standard_user')
+Password = driver.find_element(By.ID, 'password').send_keys('secret_sauce')
+login_button = driver.find_element(By.ID, 'login-button')
 login_button.click()
 
  # Добавление товаров в корзину
-backpack_add_to_cart_button = driver.find_element(By.ID, "#add-to-cart-sauce-labs-backpack")
+backpack_add_to_cart_button = driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack")
 tshirt_add_to_cart_button = driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt")
 onesie_add_to_cart_button = driver.find_element(By.ID, "add-to-cart-sauce-labs-onesie")
         
 backpack_add_to_cart_button.click()
 tshirt_add_to_cart_button.click()
 onesie_add_to_cart_button.click()
+
+# Переход в корзину
+cart_link = driver.find_element(By.CLASS_NAME, "shopping_cart_link")
+cart_link.click()
+# Нажатие кнопки Checkout
+checkout_button = driver.find_element(By.ID, "checkout")
+checkout_button.click()
+# Получение локаторов
+first_name = driver.find_element(By.ID, "first-name")
+last_name = driver.find_element(By.ID, "last-name")
+postal_code = driver.find_element(By.ID, "postal-code")
+continue_button = driver.find_element(By.ID, "continue")
+
+# Заполнение формы
+first_name.send_keys('Елена')
+last_name.send_keys('Семенкова')
+postal_code.send_keys('162250')
+
+# Переход на итоговый экран
+continue_button.click()
+
+# получение текста с итоговой суммой
+total_price_text = driver.find_element(By.CLASS_NAME, 'summary_total_label').text
+
+# поиск итоговой суммы в тексте с итоговой суммой
+assert '$58.29' in total_price_text, f"Полученный тотал: {total_price_text}, ожидаемая сумма: $58.29."
+
+driver.quit()
