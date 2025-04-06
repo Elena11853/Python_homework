380dcfcc33b67f38d8ff863bf062592ef2b5f505
Elena <beklin24@gmail.com>
2025-04-06 17:03:32 +0300

    дз 7 добавлен тест

--- 07_lesson/test_1.py
+++ 07_lesson/test_1.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 from calculator_pages import Calculator_pages
-
-
 def test_1():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     calculator_pages = Calculator_pages(driver)
@@ -11,4 +10,7 @@ def test_1():
     calculator_pages.set_time(45)
     calculator_pages.clicking()
     calculator_pages.waiting_result()
-    
+
+    result= driver.find_element(By.CSS_SELECTOR, ".screen").text
+    assert int(result) == 15, "Неверный результат"
+    print(result)
--- 07_lesson/test_2.py
+++ 07_lesson/test_2.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 from login import Login
 from main_page import Main_page
 from cart_checkout import Cart_checkout
@@ -19,4 +20,5 @@ def test_2():
     total_cost  = Total_label(driver)
     total_cost.Total_label()
 
-    
+    total_price_text = driver.find_element(By.CLASS_NAME, 'summary_total_label').text
+    assert '$58.29' in total_price_text, f"Полученный тотал: {total_price_text}, ожидаемая сумма: $58.29."
